0e25019947a02e860e58e40d49d01654fe8f81f9
Seweryn <sewcio0212@gmail.com>
2024-04-03 09:12:48 +0200

    finished

---
+++ js/01-gallery.js
@@ -0,0 +1,49 @@
+import { galleryItems } from "./gallery-items.js";
+
+// Change code below this line
+
+console.log(galleryItems);
+const link = document.querySelector(".gallery");
+
+for (const obj of galleryItems) {
+  const div = document.createElement("div");
+  div.classList.add("gallery__item");
+
+  const anchor = document.createElement("a");
+  anchor.classList.add("gallery__link");
+  anchor.setAttribute("href", obj.original);
+
+  const image = document.createElement("img");
+  image.classList.add("gallery__image");
+  image.src = obj.preview;
+  image.setAttribute("data-source", obj.original);
+  image.alt = obj.description;
+
+  link.appendChild(div);
+  div.appendChild(anchor);
+  anchor.appendChild(image);
+}
+
+link.addEventListener("click", showImage);
+
+function showImage(event) {
+  event.preventDefault();
+  const instance = basicLightbox.create(
+    `<img src='${event.target.dataset.source}'/>`,
+    {
+      onShow: () => {
+        document.addEventListener("keydown", closeModal);
+      },
+      onClose: () => {
+        document.removeEventListener("keydown", closeModal);
+      },
+    }
+  );
+  instance.show();
+
+  function closeModal(event) {
+    if (event.key === "Escape") {
+      instance.close();
+    }
+  }
+}
---
+++ js/02-lightbox.js
@@ -0,0 +1,32 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+console.log(galleryItems);
+const link = document.querySelector(".gallery");
+
+for (const obj of galleryItems) {
+  const li = document.createElement("li");
+  li.classList.add("gallery__item");
+
+  const anchor = document.createElement("a");
+  anchor.classList.add("gallery__link");
+  anchor.setAttribute("href", obj.original);
+
+  const image = document.createElement("img");
+  image.classList.add("gallery__image");
+  image.src = obj.preview;
+  image.setAttribute("data-source", obj.original);
+  image.title = obj.description;
+
+  link.appendChild(li);
+  li.appendChild(anchor);
+  anchor.appendChild(image);
+}
+
+var lightbox = new SimpleLightbox(".gallery a", { captionDelay: 250 });
+
+link.addEventListener("click", (event) => {
+  if (event.target.tagName === "IMG") {
+    event.preventDefault();
+  }
+});
